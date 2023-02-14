b74a1b5eece7d90297fe6782d8f1c9e6136c5886
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-14 14:40:24 -0300

    Enviando conceitos

---
+++ ECMAScript 6-8/JSONs encurtados/app.js
@@ -0,0 +1,24 @@
+var nome =  "Guilherme Libório";
+var idade = 21;
+var empresa = "Apple";
+
+// ! Antigo porem funcional.
+var jsonPadrao = {
+    // Normalmente seria feito assim:
+    nome: nome,
+    idade: idade,
+    empresa: empresa
+}
+console.log("MEU JSON PADRÃO ABAIXO: ")
+console.log(jsonPadrao);
+
+
+// ! Novo e também funcional.
+var jsonReduzido = { // **  Isso só funciona com var, let e const, se tentar passar um valor em string e etc não vai funcionar!
+    nome,
+    idade,
+    empresa,
+}
+
+console.log("MEU JSON REDUZIDO ABAIXO: ")
+console.log(jsonReduzido);
