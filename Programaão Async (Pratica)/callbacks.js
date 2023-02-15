026557e855abe8e5a1770474ecd043d1575767d1
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-15 18:15:22 -0300

    update callbacks

--- Programaão Async (Pratica)/callbacks.js
+++ Programaão Async (Pratica)/callbacks.js
@@ -1,13 +1,9 @@
 // ! Vamos entender como funciona o CallBack na programação async
 
-// ** O que é callback = 
-
-/*
-
 function sendEmail(msg, to, callback) {
     setTimeout(() => {
         console.log(`
-        
+
         Para: ${to}
 
         -------------------------------------
@@ -15,9 +11,9 @@ function sendEmail(msg, to, callback) {
         Mensagem: ${msg}
 
         -------------------------------------
-        
+
         De: Guilherme Libório Machado
-        
+
         `)
 
         callback(); // Esse terceiro paramêtro basicamente é uma função que ao ser chamada no output pode incluir tudo o que você quer que seja executado depois do "envio" do e-mail por exemplo.
@@ -33,7 +29,6 @@ sendEmail("Olá só estou fazendo um teste de callback com programaçao async", 
 
 }); // percecba que aqui eu defino o callback como o terceiro parâmetro logo, tudo que estiver dentro dessa função sera executado depois do "envio" do e-email
 
-*/
 
 // ** //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -43,7 +38,7 @@ function sendEmail2(msg2, to2, creator, callback2,) {
     setTimeout(() => {
 
         console.log(`
-        
+
         Para: ${to2}
 
         -------------------------------------
@@ -65,11 +60,11 @@ console.log("Inicio do teste");
 sendEmail2("Usando parâmetros no callback", "callback@teste.com", "gui", (status, amount, time, nome) => {
 
     nome = "devsupremo"
-    
+
     console.log("Teste com parâmetros feito!")
 
     console.log(`
-    
+
     - Status do envio: ${status}
     - Quantas pessoas receberam o email: ${amount} pessoas
     - Qual foi o tempo de envio: ${time}
@@ -77,4 +72,45 @@ sendEmail2("Usando parâmetros no callback", "callback@teste.com", "gui", (statu
     Feito por: ${nome}
 
     `);
+
 }); // Então deu pra entender que conseguimos passar parãmetros dinamicamente direto de uma função async para o callback.
+
+
+
+// ** //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// ! Tratamento de erros com callback e programação async
+
+function errorTest(msgTeste, agencia, callback) {
+
+    setTimeout(() => {
+
+        // ....... logica
+
+
+        // Tratamento de erro parte 1
+        var seError = false;
+
+        if (seError == true) {
+            callback(3, "O envio da mensagem falhou!!");
+
+        } else {
+            callback(3);
+        }
+
+    }, 3000);
+}
+
+
+// output
+console.log("Inicio do teste");
+errorTest("mensagem do teste enviada!", "desenvolvimento", (time, erro) => {
+
+    // Tratamento de erro parte 2
+    if (erro == undefined) {
+        console.log(`Teste concluido, tempo de execução: ${time}s`);
+    } else {
+        console.log(`Ocorreu um erro: ${erro}, tempo de execução: ${time}`);
+    }
+
+});
