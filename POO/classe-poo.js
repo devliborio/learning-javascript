922ce74b2194ebaf57fd9688273fe92997f63875
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-17 15:33:23 -0300

    Estrutura de uma classe em POO

---
+++ POO/classe-poo.js
@@ -0,0 +1,25 @@
+// ** Estrutura de uma classe em POO
+
+class Jogo{
+
+    constructor(){
+        this.titulo="Elden ring",
+        this.genero = "Souls like",
+        this.produtora = "From Software",
+        this.preco = "R$ 249,00"
+    }
+
+    // Métodos
+
+    Abrir(){
+        console.log("Jogo abrindo")
+    }
+
+    Carregar(){
+        console.log("Carregando...")
+    }
+
+    Fechar(){
+        console.log("Aperte ESC para fechar...")
+    }
+}
