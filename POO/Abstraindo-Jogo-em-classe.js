7edaecc0069c9509cbfda9f152d1c91555d4cd85
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-17 16:19:59 -0300

    Abstraindo conceitos

---
+++ POO/Abstraindo-Jogo-em-classe.js
@@ -0,0 +1,28 @@
+class Jogo{
+
+    constructor(){
+        // Atributos
+        this.nome = '',
+        this.genero = '',
+        this.preco = '',
+        this.distribuidora = ''
+    }
+
+    //Métodos
+
+    Executar(){
+        console.log('executando...');
+    }
+
+    Salvar(){
+        console.log('salvando...');
+    }
+
+    Atualizar(){
+        console.log('atualizando...')
+    }
+
+    Fechar(){
+        console.log('fechando...')
+    }
+}
