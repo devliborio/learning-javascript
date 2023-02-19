ceceff2e8e025d3c2e900bd18965b32abf11299f
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-19 00:46:16 -0300

    Abstraindo conceitos

---
+++ POO/Abstraindo-CursoUdemy.js
@@ -0,0 +1,38 @@
+class CursoUdemy {
+
+    constructor() {
+
+        //Atributos
+        this.nome = 'Formação Nodejs';
+        this.instrutor = 'Victor';
+        this.preço = 'R$...',
+        this.certificado = 'nome.....';
+        this.categoria = 'Desenvolvimento'
+
+    }
+    //Métodos
+
+    Comprar() {
+        console.log('Comprando...');
+    }
+
+    Assistir() {
+        console.log('Assistindo...');
+    }
+
+    Reembolsar() {
+        console.log('Reembolsando...');
+    }
+
+    Pausar() {
+        console.log('Pausando...');
+    }
+
+    Iniciar() {
+        console.log('Iniciando...');
+    }
+
+    Avançar() {
+        console.log('Avançando...');
+    }
+}
