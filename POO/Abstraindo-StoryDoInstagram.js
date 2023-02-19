8fef512a2bacdf072300a6b86d7fe7f20b448464
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-19 01:31:50 -0300

    abstraindo conceitos

---
+++ POO/Abstraindo-StoryDoInstagram.js
@@ -0,0 +1,38 @@
+class StoryInstagram{
+
+    constructor(){
+
+        //Atributos
+        this.efeitos =[],
+        this.foto = '',
+        this.comentário= '',
+        this.reação = ''
+        this.musica = ''
+    }
+
+    //Métodos
+    postar(){
+        console.log('postando...');
+    }
+
+    apagar(){
+        console.log('apagando...');
+    }
+
+    destacar(){
+        console.log('destacar');
+    }
+
+    enviarPara(){
+        console.log('enviando...');
+    }
+
+    comentar(){
+        console.log('comentando...');
+    }
+
+    marcarPessoa(){
+        console.log('marcando alguém!');
+    }
+
+}
