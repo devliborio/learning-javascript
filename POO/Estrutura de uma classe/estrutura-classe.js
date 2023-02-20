a1a2ad433c699dd5378b7246522b4944eaa4be1f
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-20 10:44:21 -0300

    Adding Class Structure

---
+++ POO/Estrutura de uma classe/estrutura-classe.js
@@ -0,0 +1,12 @@
+class NomeDaClasse{
+
+    constructor(){
+        /*Atributos
+        this.name= '', [], 12, {}...
+        */
+    }
+
+    /* Métodos
+    qualquerAção(){ o que a ação faz! }
+    */
+}
