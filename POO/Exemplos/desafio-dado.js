fe39367d084b0b39510e2cd6d5fcf191620e53a5
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-20 15:05:03 -0300

    update

--- POO/Exemplos/desafio-dado.js
+++ POO/Exemplos/desafio-dado.js
@@ -6,7 +6,7 @@ class Dado {
 
     // Métodos
     Rolar() {
-        console.log('Resultado do dado ' + this.GetRandomIntInclusive(1,this.faces));
+        console.log('Resultado do dado ' + this.GetRandomIntInclusive(1, this.faces));
     }
 
     // Função de gerar número aleatorios
@@ -19,11 +19,13 @@ class Dado {
 
 let d6 = new Dado(6);
 let d12 = new Dado(12);
+let d20 = new Dado(20);
 let d100 = new Dado(100);
 let d200 = new Dado(200);
 
 //Output
 d6.Rolar();
 d12.Rolar();
+d20.Rolar();
 d100.Rolar();
 d200.Rolar();
